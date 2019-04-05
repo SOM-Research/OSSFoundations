f23b7f412825d84140596f5e46d73299afe1921f
Javier Canovas <me@jlcanovas.es>
2019-04-05 12:54:13 +0200

    Things changed for alignment with journal

--- js/app.js
+++ js/app.js
@@ -17,11 +17,11 @@ let vm = new Vue({
     search: '',                // Keyword to search
     selectedCategory: -1,      // Category selected in the graph (as index)
     selectedCategoryValue: '', // Value on the category selected
-    formQ1: '',                // Answer for Q1 in the form
-    formQ1topics: false,       // Answer for Q1 topics in the form
-    formQ2geo: '',             // Answer for Q2GEO in the form
-    formQ2cov: '',             // Answer for Q2COV in the form
-    formQ2mis: '',             // Answer for Q2MIS in the form
+    formNatureInter: '',       // Answer for Q1Inter in the form
+    formNatureIndep: '',       // Answer for Q1Indep in the form
+    formNatureOpen: '',        // Answer for Q1Open  in the form
+    formSD: '',                // Answer for SD in the form
+    formTopics: false,         // Answer for topics in the form
     timer: 0,                  // Last timer created (to dealy the graph update)
     previousList: [],          // Cached result to be used when clicking on the graph consecutively
     selectedTopics: [],        // Topics selected
@@ -58,25 +58,25 @@ let vm = new Vue({
     filteredOrderedList : function() {
       var self = this
       
-      // Click on the graph apply filters consecutively
+      // Click on the graph apply filters consecutively (removed)
       var prefiltered = self.foundations
-      if(self.selectedCategory > -1) {
+      /*if(self.selectedCategory > -1) {
           prefiltered = self.previousList.filter(function(foundation) {
             return (self.selectedCategory > -1 ? self.mapCategoryValue(foundation, self.selectedCategory) == self.selectedCategoryValue : true)
           })
-      }
+      }*/
 
       // Filtering according to keyword and form
       var filtered = prefiltered.filter(function(foundation) {
         return foundation.name.toLowerCase().indexOf(self.search.toLowerCase()) !== -1 &&
-               (self.formQ1 != '' ? foundation.rq1 == self.formQ1 : true) &&
-               (self.formQ2geo ? foundation.rq2geo == 'Y' : true) &&
-               (self.formQ2cov ? foundation.rq2cov == 'Y' : true) &&
-               (self.formQ2mis ? foundation.rq2mis == 'Y' : true) 
+               (self.formSD != '' ? foundation.SD == self.formSD : true) &&
+               (self.formNatureInter ? foundation.rq1Inter == 'Y' : true) &&
+               (self.formNatureIndep ? foundation.rq1Indep == 'Y' : true) &&
+               (self.formNatureOpen  ? foundation.rq1Open == 'Y' : true) 
       })
 
       // Topic filtering
-      if(self.formQ1topics) {
+      if(self.formTopics) {
         filtered = filtered.filter(function(foundation) {
           if(self.selectedTopics.length == 0) return false
           for(i in self.selectedTopics) 
@@ -90,19 +90,17 @@ let vm = new Vue({
       var ordered = _.orderBy(filtered, self.sortKey, (self.reverse ? 'desc' : 'asc'))
 
       // We build the data to render in the graph
-      var rq1Y     = ordered.filter(function(f) { return f.rq1 == "Y";}).length
-      var rq1N     = ordered.filter(function(f) { return f.rq1 == "N";}).length
-      var rq2geoY  = ordered.filter(function(f) { return f.rq2geo == "Y";}).length
-      var rq2geoN  = ordered.filter(function(f) { return f.rq2geo == "N";}).length
-      var rq2covY  = ordered.filter(function(f) { return f.rq2cov == "Y";}).length
-      var rq2covN  = ordered.filter(function(f) { return f.rq2cov == "N";}).length
-      var rq2misY  = ordered.filter(function(f) { return f.rq2mis == "Y";}).length
-      var rq2misN  = ordered.filter(function(f) { return f.rq2mis == "N";}).length
-      /*var rq3rq4Y  = ordered.filter(function(f) { return f.rq3rq4 == "Y";}).length
-      var rq3rq4NA = ordered.filter(function(f) { return f.rq3rq4 == "-";}).length*/
+      var rqNatureInterY  = ordered.filter(function(f) { return f.rq1Inter == "Y";}).length
+      var rqNatureInterN  = ordered.filter(function(f) { return f.rq1Inter == "N";}).length
+      var rqNatureIndepY  = ordered.filter(function(f) { return f.rq1Indep == "Y";}).length
+      var rqNatureIndepN  = ordered.filter(function(f) { return f.rq1Indep == "N";}).length
+      var rqNatureOpenY   = ordered.filter(function(f) { return f.rq1Open == "Y";}).length
+      var rqNatureOpenN   = ordered.filter(function(f) { return f.rq1Open == "N";}).length
+      var rqSDY     = ordered.filter(function(f) { return f.SD == "Y";}).length
+      var rqSDN     = ordered.filter(function(f) { return f.SD == "N";}).length
 
-      selected   = ["selected", rq1Y, rq2geoY, rq2covY, rq2misY] /*, rq3rq4Y]*/
-      unselected = ["unselected", rq1N, rq2geoN, rq2covN, rq2misN]/*, 0]*/
+      selected   = ["selected",   rqNatureInterY, rqNatureIndepY, rqNatureOpenY, rqSDY] /*, rq3rq4Y]*/
+      unselected = ["unselected", rqNatureInterN, rqNatureIndepN, rqNatureOpenN, rqSDN]/*, 0]*/
 
       self.timer = setTimeout(function(newColumns) {
         self.chart.load({ columns: [selected, unselected], unload: true })
@@ -120,25 +118,24 @@ let vm = new Vue({
       bindto: '#chart',
       data: {
         columns: [
-          ['selected', 65, 47, 51, 37],/*, 18],*/
-          ['unselected', 24, 18, 14, 28]/*, 0]*/
+          ['selected', 65, 47, 51, 37],
+          ['unselected', 24, 18, 14, 28]
         ],
         type: 'bar',
-        onclick: function (d, element) { 
+        /*onclick: function (d, element) { 
           self.selectedCategory = d.index;
           self.selectedCategoryValue = d.name;
           self.updateForm(d.index, d.name);
-        },
+        },*/
       },
       axis : {
         x : {
           type: 'category',
           categories : [
-            'Software Product Supporter', 
             'International Scope', 
             'Independent', 
-            'Clear mission'/*, */
-            /*'Selected'*/
+            'Transparent',
+            'Software Product Supporter'
           ]
         }
       }
@@ -153,10 +150,10 @@ let vm = new Vue({
     reset: function() {
       var self = this
       self.search = ''
-      self.formQ1 = ''
-      self.formQ2geo = ''
-      self.formQ2cov = ''
-      self.formQ2mis = ''
+      self.formSD = ''
+      self.formNatureInter = ''
+      self.formNatureIndep = ''
+      self.formNatureOpen  = ''
       self.selectedCategory = -1
       self.selectedCategoryValue = ''
       $('.collapse-foundation').collapse('hide')
@@ -164,14 +161,14 @@ let vm = new Vue({
     },
     resetTopics: function() {
       var self = this
-      self.formQ1topics = false
+      self.formTopics = false
       $('.collapse-topics').collapse('hide')
       self.resetTopicList()
       $('.btn-topic').button('dispose')
     },
     resetTopicQuestion: function() {
       var self = this
-      self.formQ1topics = false
+      self.formTopics = false
       $('#topics-list').collapse('hide')
       self.resetTopicList()
     },
@@ -182,9 +179,9 @@ let vm = new Vue({
     },
     getCategoryValue: function(foundation, category) {
       if(category == 0) return foundation.rq1;
-      else if(category == 1) return foundation.rq2geo;
-      else if(category == 2) return foundation.rq2cov;
-      else if(category == 3) return foundation.rq2mis;
+      else if(category == 1) return foundation.rq1Inter;
+      else if(category == 2) return foundation.rq1Indep;
+      else if(category == 3) return foundation.rq1Inter;
       else if(category == 4) return foundation.rq3rq4;
     },
     mapCategoryValue: function(foundation, category) { 
@@ -197,51 +194,59 @@ let vm = new Vue({
     updateForm: function(category, categoryValue) {
       var self = this
       if(category == 0) {
-        if(categoryValue == "selected") self.formQ1 = 'Y'
-        else if(categoryValue == "unselected") self.formQ1 = 'N'
+        if(categoryValue == "selected") self.formSD = 'Y'
+        else if(categoryValue == "unselected") self.formSD = 'N'
       }
       else if(category == 1) {
-        if(categoryValue == "selected") self.formQ2geo = true
-        else if(categoryValue == "unselected") self.formQ2geo = false
+        if(categoryValue == "selected") self.formNatureInter = true
+        else if(categoryValue == "unselected") self.formNatureInter = false
       }
       else if(category == 2) {
-        if(categoryValue == "selected") self.formQ2cov = true
-        else if(categoryValue == "unselected") self.formQ2cov = false
+        if(categoryValue == "selected") self.formNatureIndep = true
+        else if(categoryValue == "unselected") self.formNatureIndep = false
       }
       else if(category == 3) {
-        if(categoryValue == "selected") self.formQ2mis = true
-        else if(categoryValue == "unselected") self.formQ2mis = false
+        if(categoryValue == "selected") self.formNatureOpen = true
+        else if(categoryValue == "unselected") self.formNatureOpen = false
       }
     },
     toggleTopic: function(topic) {
       var self = this
       if(self.selectedTopics.indexOf(topic) == -1) {
         self.selectedTopics.push(topic)
+        if(topic == 'Software-Development') {
+          self.formSD = 'Y'
+        }
       } else {
         self.selectedTopics = self.selectedTopics.filter(function(value, index, arr){
           return value != topic;
         });
+        if(topic == 'Software-Development') {
+          self.formSD = 'N'
+        }
       }
     },
     toggleQ1topics: function(show) {
       var self = this
       if(show) {
-        self.formQ1topics = true
+        self.formTopics = true
         $('#topics-list').collapse('show')
       } else {
-        self.formQ1topics = false
+        self.formTopics = false
         $('#topics-list').collapse('hide')
         self.resetTopicQuestion()
       }
     },
-    toggleQ1: function(answer) {
+    toggleSD: function(answer) {
       var self = this
       if(answer) {
-        self.formQ1 = 'Y'
-        self.resetTopics()
+        self.formSD = 'Y'
+        self.selectedTopics.push('Software-Development')
       } else {
-        self.formQ1 = 'N'
-        $('#topics-question').collapse('show')
+        self.formSD = 'N'
+        self.selectedTopics = self.selectedTopics.filter(function(value, index, arr){
+          return value != 'Software-Development';
+        });
       }
     }
   }
